6f3a9c4d92db45b56ce0f002a4224adbda8045ea
saurabh-44 <938saurabh@gmail.com>
2025-07-24 12:00:28 +0530

    create course model and CRUD APIs

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({});
 
+
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
         method:`${method}`,
