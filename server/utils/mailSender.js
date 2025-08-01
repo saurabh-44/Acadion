276572856632988d4a6a2e24f3a1dc34496feda4
saurabh-44 <938saurabh@gmail.com>
2025-08-01 19:03:49 +0530

    Add resend verification email feature

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: `"Study Notion" <${process.env.MAIL_USER}>`,
+                from: `"Acadion" <${process.env.MAIL_USER}>`,
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
