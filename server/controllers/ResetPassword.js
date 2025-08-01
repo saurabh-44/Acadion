0991ad00f25a26c9a1725760c8150afc8db836c3
saurabh-44 <938saurabh@gmail.com>
2025-08-01 16:42:41 +0530

    Add forgot password and reset password flow

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 
 exports.resetPasswordToken = async (req, res) => {
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://studynotion.fun/update-password/${token}`;
 
 		await mailSender(
 			email,
