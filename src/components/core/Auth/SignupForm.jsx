8c56a6cb4e4be6cf191f414f62cf05fd2c51bc26
saurabh-44 <938saurabh@gmail.com>
2025-08-02 23:09:38 +0530

    Add Authentication pages

--- src/components/core/Auth/SignupForm.jsx
+++ src/components/core/Auth/SignupForm.jsx
@@ -8,6 +8,7 @@ import { sendOtp } from "../../../services/operations/authAPI"
 import { setSignupData } from "../../../slices/authSlice"
 import { ACCOUNT_TYPE } from "../../../utils/constants"
 import Tab from "../../common/Tab"
+import {setProgress} from "../../../slices/loadingBarSlice"
 
 function SignupForm() {
   const navigate = useNavigate()
@@ -197,7 +198,7 @@ function SignupForm() {
           </label>
         </div>
         <button
-          type="submit"
+          type="submit" onClick={()=>{dispatch(setProgress(60))}}
           className="mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900"
         >
           Create Account
